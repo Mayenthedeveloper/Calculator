4fe006289cbc693938774dfa5f7ab57b66c01d13
Mayen Akpan <neroxma@yahoo.com>
2021-05-22 16:19:35 -0700

    Added styles

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -9,23 +9,25 @@ export default function Calculator() {
         <Current>10</Current>
       </Screen>
       <Button gridSpan={2}>AC</Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button gridSpan={2}>=</Button>
+      <Button control>DEL</Button>
+      <Button operation>÷</Button>
+      <Button>7</Button>
+      <Button>8</Button>
+      <Button>9</Button>
+      <Button operation>x</Button>
+      <Button>4</Button>
+      <Button>5</Button>
+      <Button>6</Button>
+      <Button operation>+</Button>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+      <Button operation>-</Button>
+      <Button period>.</Button>
+      <Button>0</Button>
+      <Button gridSpan={2} equals>
+        =
+      </Button>
     </Container>
   );
 }
