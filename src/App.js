101d80a0d7dcb31ae876bb13ce6a243d21002d0a
Mayen Akpan <neroxma@yahoo.com>
2021-05-22 14:59:31 -0700

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome</h1>
-      </header>
+    <div>
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/Calculator/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Screen, Previous, Current, Button } from "./Styled";
+
+export default function Calculator() {
+  return (
+    <Container>
+      <Screen>
+        <Previous>10 +</Previous>
+        <Current>10</Current>
+      </Screen>
+      <Button gridSpan={2}>AC</Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button gridSpan={2}>=</Button>
+    </Container>
+  );
+}
